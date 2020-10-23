848b8e012954bc80456c0d966a8a430605a622af
wojteky2000 <wojciechbernat200006@gmail.com>
2020-10-23 10:40:29 +0200

    Commit 1

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,12 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
     public static void main(String [ ] args) {
+        Logger log = Logger.getLogger(HelloWorld.class);
+        log.info("message");
         System.out.println("Hello World");
     }
 }
